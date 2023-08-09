c0d07539706c4b29dbe966853df56111a9baed60
Ryan Stewart <ryan86stewart@gmail.com>
2023-08-09 12:45:12 -0500

    Implement Homepage (#5)
    * Implemented Firebase Auth
    
    * Implement Firebase Storage service
    
    * Implement Homepage

---
+++ src/components/ArtistProfile/ArtistSpotlight.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ArtistSpotlight = () => {
+  // For now, we'll just display a static message.
+  // Later on, you can fetch artist information from your database and spotlight them.
+
+  return (
+    <section>
+      <h2>Artist Spotlight</h2>
+      <p>Discover our featured artist of the month and dive deep into their creative journey.</p>
+    </section>
+  );
+};
+
+export default ArtistSpotlight;
